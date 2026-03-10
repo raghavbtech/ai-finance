40fa35d732ae767fb7a38aeead30fb8773213c7f
Raghav Chugh <raghav.cseai@gmail.com>
2026-03-10 20:43:33 +0530

    Fixed frontend

--- backend/finance_ai/settings.py
+++ backend/finance_ai/settings.py
@@ -64,6 +64,9 @@ DATABASES = {
         'PASSWORD': config('DB_PASSWORD'),
         'HOST': config('DB_HOST', default='localhost'),
         'PORT': config('DB_PORT', default='5432'),
+        'OPTIONS': {
+            'sslmode': config('DB_SSLMODE', default='prefer'),
+        },
     }
 }
 
